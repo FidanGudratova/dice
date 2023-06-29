0f99dbb0a5a05c42137f36beccd9ef5683725d87
FidanGudratova <fqudretova@gmail.com>
2023-06-29 17:03:54 +0400

    2906

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
-function rollDice() {
-    let diceResult = Math.floor(Math.random() * 6) + 1;
+ function rollDice () {
+   
+    const oneResult = document.querySelector('.dot');
+    const resultOne = Math.floor(Math.random() * 6) + 1;
+    oneResult.innerText = resultOne;
+
+    const twoResult = document.querySelector('.dot-two');
+    const resultTwo = Math.floor(Math.random() * 6 + 1);
+    twoResult.innerText = resultTwo;
+
+    const sumResult = document.querySelector('.diceResult');
+    sumResult.innerText = resultTwo + resultOne;
 }
+
+
   
